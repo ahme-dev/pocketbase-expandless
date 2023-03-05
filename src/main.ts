03f04c18f1478b83f5fd651a1f5390d01f3cf7ed
Ahmed K.A <ahmadkabdullah@pm.me>
2023-03-05 19:28:21 +0300

    Move code to TS and restructure the library

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { Record } from "pocketbase";
+
+// A Record type without the expand property
+type RecordExpandless = Omit<Record, "expand">;
+
 // takes in a record or a list of records
 // recursivly moves all the properties in the expand property to the record itself
 // i.e product.expand.category_id -> product.category_id
 // returns a record or a list of records
-export function moveExpandsInline(records) {
+export function moveExpandsInline(record: Record[] | Record): RecordExpandless {
 	// clone record
-	let newRecord = JSON.parse(JSON.stringify(records));
+	let newRecord: RecordExpandless = JSON.parse(JSON.stringify(record));
 
 	// if got an array of records
 	if (Array.isArray(newRecord)) {
@@ -12,7 +17,7 @@ export function moveExpandsInline(records) {
 		for (let i = 0; i < newRecord.length; i++) {
 			console.log("Running in array");
 			// run expandRelations on each record
-			newRecord[i] = moveExpandsInline(newRecord[i]);
+			newRecord[i] = moveExpandsInline(newRecord[i]) as Record;
 		}
 	}
 
@@ -25,7 +30,7 @@ export function moveExpandsInline(records) {
 		newRecord[key] = newRecord.expand[key];
 
 		// run expandRelations on expanded record to find nested expand record
-		newRecord[key] = moveExpandsInline(newRecord[key]);
+		newRecord[key] = moveExpandsInline(newRecord[key]) as Record;
 	}
 
 	// remove expand field from record
